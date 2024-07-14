d77a9c7e6c9f8c2b7a0e571ec4d338fd89ee4154
Nazlı Yıldırım <nazliyil87@gmail.com>
2024-07-14 18:20:38 +0300

    V.2
    asda

--- Ticaret.Application/Class1.cs
+++ Ticaret.Application/Class1.cs
@@ -2,6 +2,34 @@
 {
     public class Class1
     {
+        public class Class1
+        {
+            // Sınıf özellikleri
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public DateTime CreatedDate { get; set; }
 
+            // Sınıfın bir constructor'ı
+            public Class1(int id, string name, DateTime createdDate)
+            {
+                Id = id;
+                Name = name;
+                CreatedDate = createdDate;
+            }
+
+            // Sınıfın bir metodu
+            public void DisplayInfo()
+            {
+                Console.WriteLine($"Id: {Id}");
+                Console.WriteLine($"Name: {Name}");
+                Console.WriteLine($"Created Date: {CreatedDate.ToShortDateString()}");
+            }
+
+            // Sınıfın başka bir metodu
+            public string GetFormattedInfo()
+            {
+                return $"Class1 - Id: {Id}, Name: {Name}, Created Date: {CreatedDate.ToShortDateString()}";
+            }
+        }
     }
 }
